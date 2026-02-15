d938d86dd3da67b7766e15f5324bffe56f4902c8
Entrepreneur <entrepreneur84@bk.ru>
2026-02-15 14:44:23 +0700

    Add CSV export function to utils

--- workers/utils/csvExport.js
+++ workers/utils/csvExport.js
@@ -1,1 +1,9 @@
-
+export function exportCSV(data){
+  if(!data || !data.length) return ""
+  const keys = Object.keys(data[0])
+  const csv = [keys.join(",")]
+  data.forEach(row=>{
+    csv.push(keys.map(k=>row[k]).join(","))
+  })
+  return csv.join("\n")
+}
