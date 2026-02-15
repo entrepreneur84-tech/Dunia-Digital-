21666bc979bf1cf7db2e10d83fa323cf6d23e60b
Entrepreneur <entrepreneur84@bk.ru>
2026-02-15 14:42:49 +0700

    Add invoiceTemplate function for generating invoices

--- workers/utils/invoiceTemplate.js
+++ workers/utils/invoiceTemplate.js
@@ -1,1 +1,8 @@
-
+export function invoiceTemplate({email,plan,invoiceNo}){
+  return `
+    <h3>Invoice ${invoiceNo}</h3>
+    <p>Email: ${email}</p>
+    <p>Paket: ${plan}</p>
+    <p>Silakan bayar via Bank atau Bitcoin/Ethereum. Konfirmasi WA/Email untuk aktivasi license.</p>
+  `
+}
