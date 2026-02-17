53e17f43e03de39e3c588401f1359da2b42232e6
Entrepreneur <entrepreneur84@bk.ru>
2026-02-17 18:53:11 +0700

    Update products.js
    Signed-off-by: Entrepreneur  <entrepreneur84@bk.ru>

--- functions/api/products.js
+++ functions/api/products.js
@@ -1,1 +1,11 @@
+export async function onRequest() {
+  const products = [
+    { id: "ebook1", title: "Kesadaran Diri Spiritual", price: 100000 },
+    { id: "ebook2", title: "Makna Hidup & Kesadaran Jiwa", price: 200000 },
+    { id: "bundle", title: "Bundle Spiritual Premium + Sistem", price: 250000 }
+  ];
 
+  return new Response(JSON.stringify(products), {
+    headers: { "Content-Type": "application/json" }
+  });
+}
